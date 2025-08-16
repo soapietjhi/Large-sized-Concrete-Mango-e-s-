cef10f8276fdd1497e36eba941b5db2e27c3e545
Sophie Tjhi <sophie.tjhi@gmail.com>
2025-08-16 15:49:45 +0800

    Gallery, Inventory, and move camera to carrotView

---
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/carrotView.swift
@@ -0,0 +1,18 @@
+//
+//  carrotView.swift
+//  Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭
+//
+//  Created by Sophie Tjhi on 16/8/25.
+//
+
+import SwiftUI
+
+struct carrotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    carrotView()
+}
