03e285a5b7326f0c8837ad197b7885f14d4b958e
Sophie <lian_pei_ying_sophie_2028@mgs.sch.edu.sg>
2025-08-16 14:08:28 +0800

    new swift file

---
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/CameraViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewModel.swift
+//  Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭
+//
+//  Created by Sophie Lian on 16/8/25.
+//
+
+import SwiftUI
+import AVFoundation
+import Vision
+
+@Observable
+class
