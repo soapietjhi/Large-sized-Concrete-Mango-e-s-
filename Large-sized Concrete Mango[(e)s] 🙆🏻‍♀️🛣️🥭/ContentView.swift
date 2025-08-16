375af4b8c4d3b6916835a4433ca2007f3bd6afbc
Sophie Tjhi <sophie.tjhi@gmail.com>
2025-08-16 13:46:16 +0800

    Initial Commit

---
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭
+//
+//  Created by Sophie Tjhi on 16/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/Large_sized_Concrete_Mango__e_s______________App.swift
@@ -0,0 +1,17 @@
+//
+//  Large_sized_Concrete_Mango__e_s______________App.swift
+//  Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭
+//
+//  Created by Sophie Tjhi on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Large_sized_Concrete_Mango__e_s______________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
