1fa900fffcbe1d47c391780c0b587bce479fabff
Sophie <lian_pei_ying_sophie_2028@mgs.sch.edu.sg>
2025-09-12 11:57:54 +0800

    wallpaper ayyayyayyaya

--- Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/potatoView.swift
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/potatoView.swift
@@ -15,113 +15,128 @@ struct potatoView: View {
     var body: some View {
         NavigationStack {
             
-            VStack {
-                Text("Click on the text to select fruits for your smoothie.")
-                    .font(.system(size: 35))
-                    .padding(30)
-            }
-            HStack (spacing: 30) {
-                VStack (spacing: 23){
-                    Text ("🍎")
-                        .font(.system(size: 50))
-                    Text ("🍌")
-                        .font(.system(size: 50))
-                    Text ("🍓")
-                        .font(.system(size: 50))
-                    Text ("🥭")
-                        .font(.system(size: 50))
-                    Text ("🫐")
-                        .font(.system(size: 50))
-                    Text ("🍑")
-                        .font(.system(size: 50))
-                }
-                VStack(spacing: 40) {
-                    Button("Apple            \(fruitsViewModel.numberofapples)"){
-                        if fruitsViewModel.numberofapples != 0 {
-                            fruitsViewModel.numberofapples -= 1
-                            Next = true
-                        }
-                        else {
-                            showAlert = true
-                        }
-                    }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
-                    Button("Banana         \(fruitsViewModel.numberofbananas)"){
-                        if fruitsViewModel.numberofbananas != 0 {
-                            fruitsViewModel.numberofbananas -= 1
-                            Next = true
-                        }
-                        else {
-                            showAlert = true
-                        }
-                    }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
+            ZStack{
+                Image("Fruit")
                     
-                    Button("Strawberry  \(fruitsViewModel.numberofstrawberries)"){
-                        if fruitsViewModel.numberofstrawberries != 0 {
-                            fruitsViewModel.numberofstrawberries -= 1
-                            Next = true
+                
+                
+                VStack {
+                    Text("Click on the text to select ")
+                        .font(.system(size: 30))
+                        .bold()
+                       
+                        
+                        
+                    Text(" fruits for your smoothie.")
+                        .font(.system(size: 30))
+                        .bold()
+                    
+                        
+                    
+                    HStack (spacing: 30) {
+                        VStack (spacing: 23){
+                            Text ("🍎")
+                                .font(.system(size: 50))
+                            Text ("🍌")
+                                .font(.system(size: 50))
+                            Text ("🍓")
+                                .font(.system(size: 50))
+                            Text ("🥭")
+                                .font(.system(size: 50))
+                            Text ("🫐")
+                                .font(.system(size: 50))
+                            Text ("🍑")
+                                .font(.system(size: 50))
                         }
-                        else {
-                            showAlert = true
+                        VStack(spacing: 40) {
+                            Button("Apple            \(fruitsViewModel.numberofapples)"){
+                                if fruitsViewModel.numberofapples != 0 {
+                                    fruitsViewModel.numberofapples -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
+                            Button("Banana         \(fruitsViewModel.numberofbananas)"){
+                                if fruitsViewModel.numberofbananas != 0 {
+                                    fruitsViewModel.numberofbananas -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
+                            
+                            Button("Strawberry  \(fruitsViewModel.numberofstrawberries)"){
+                                if fruitsViewModel.numberofstrawberries != 0 {
+                                    fruitsViewModel.numberofstrawberries -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
+                            Button("Mango          \(fruitsViewModel.numberofmangoes)") {
+                                if fruitsViewModel.numberofmangoes != 0 {
+                                    fruitsViewModel.numberofmangoes -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
+                            Button("Blueberry    \(fruitsViewModel.numberofblueberries)") {
+                                if fruitsViewModel.numberofblueberries != 0 {
+                                    fruitsViewModel.numberofblueberries -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
+                            Button("Peach          \(fruitsViewModel.numberofpeaches)"){
+                                if fruitsViewModel.numberofpeaches != 0 {
+                                    fruitsViewModel.numberofpeaches -= 1
+                                    Next = true
+                                }
+                                else {
+                                    showAlert = true
+                                }
+                            }
+                            .font(.system(size: 35))
+                            .foregroundStyle(.primary)
                         }
                     }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
-                    Button("Mango          \(fruitsViewModel.numberofmangoes)") {
-                        if fruitsViewModel.numberofmangoes != 0 {
-                            fruitsViewModel.numberofmangoes -= 1
-                            Next = true
-                        }
-                        else {
-                            showAlert = true
-                        }
-                    }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
-                    Button("Blueberry    \(fruitsViewModel.numberofblueberries)") {
-                        if fruitsViewModel.numberofblueberries != 0 {
-                            fruitsViewModel.numberofblueberries -= 1
-                            Next = true
-                        }
-                        else {
-                            showAlert = true
+                    .frame(maxWidth: .maximum(330, 100))
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(12)
+                    
+                    if Next == true {
+                        NavigationLink {
+                            BlenderView(totalSmoothies: $totalSmoothies)
+                        } label: {
+                            Text("Next")
+                                .font(.largeTitle)
+                                .frame(width: 300, height: 100, alignment: .bottomTrailing)
                         }
                     }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
-                    Button("Peach          \(fruitsViewModel.numberofpeaches)"){
-                        if fruitsViewModel.numberofpeaches != 0 {
-                            fruitsViewModel.numberofpeaches -= 1
-                            Next = true
-                        }
-                        else {
-                            showAlert = true
-                        }
-                    }
-                    .font(.system(size: 35))
-                    .foregroundStyle(.primary)
+                }
+                .alert("You do not have enough of this fruit to make a Smoothie with it", isPresented: $showAlert) {
+                    Button("Ok", role: .cancel) {}
                 }
             }
-            .frame(maxWidth: .maximum(330, 100))
-            .padding()
-            .background(Color(.systemGray6))
-            .cornerRadius(12)
-            
-            if Next == true {
-                NavigationLink {
-                    BlenderView(totalSmoothies: $totalSmoothies)
-                } label: {
-                    Text("Next")
-                        .font(.largeTitle)
-                        .frame(width: 300, height: 100, alignment: .bottomTrailing)
-                }
-            }
-        }
-        .alert("You do not have enough of this fruit to make a Smoothie with it", isPresented: $showAlert) {
-            Button("Ok", role: .cancel) {}
         }
     }
 }
