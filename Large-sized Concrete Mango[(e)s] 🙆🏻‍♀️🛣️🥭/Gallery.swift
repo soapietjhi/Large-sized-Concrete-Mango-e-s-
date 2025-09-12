8c2c7305c5ba6c9f50f07709ab6ce688dde471af
Sophie <lian_pei_ying_sophie_2028@mgs.sch.edu.sg>
2025-09-12 14:26:31 +0800

    more backgrounds yayayayyayay

--- Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/Gallery.swift
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/Gallery.swift
@@ -12,26 +12,36 @@ struct Gallery: View {
     @AppStorage("totalSmoothies") var totalSmoothies: Int = 0
     var body: some View {
         NavigationStack {
-            VStack (spacing: 25) {
-                Text("You made \(totalSmoothies) smoothies!")
-                    .font(.system(size: 50))
-                    .padding()
-                    .multilineTextAlignment(.center)
-                NavigationLink {
-                    potatoView(totalSmoothies: $totalSmoothies)
-                } label: {
-                    Label ("", systemImage: "plus")
-                        .font(.system(size:35))
-                        .foregroundStyle(.white)
-                    Text("New smoothies")
-                        .font(.system(size: 35))
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.leading)
-                }
-                .padding()
-                .background(.blue)
-                .cornerRadius(10)
+            
+            ZStack{
+                Image ("Fruit2")
                 
+                VStack (spacing: 25) {
+                    Text("You made \(totalSmoothies)")
+                        .font(.system(size: 50))
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        
+                    Text ("smoothies!")
+                        .font(.system(size: 50))
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    NavigationLink {
+                        potatoView(totalSmoothies: $totalSmoothies)
+                    } label: {
+                        Label ("", systemImage: "plus")
+                            .font(.system(size:35))
+                            .foregroundStyle(.white)
+                        Text("New smoothies")
+                            .font(.system(size: 35))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .padding()
+                    .background(.teal)
+                    .cornerRadius(10)
+                    
+                }
             }
         }
     }
--- Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/Inventory.swift
+++ Large-sized Concrete Mango[(e)s] 🙆🏻‍♀️🛣️🥭/Inventory.swift
@@ -11,72 +11,78 @@ struct Inventory: View {
     @EnvironmentObject var fruitsViewModel: FruitsViewModel
     var body: some View {
             NavigationStack {
-                VStack (spacing: 15) {
-                Text("Inventory")
-                    .font(.system(size: 35))
+                
+                ZStack{
                     
-                NavigationLink{
-                    carrotView()
-                } label: {
-                    Text("Collect more fruits")
-                }
-                .padding()
-                .font(.system(size: 27))
-                .foregroundColor(.white)
-                .frame(maxWidth: .maximum(300, 10))
-                .background(Color.blue)
-                .cornerRadius(10)
-                HStack (spacing: 30) {
-                    VStack (spacing: 20){
-                        Text("🍎")
-                            .font(.system(size: 50))
-                        Text("🍌")
-                            .font(.system(size: 50))
-                        Text("🍓")
-                            .font(.system(size: 50))
-                        Text("🥭")
-                            .font(.system(size: 50))
-                        Text("🫐")
-                            .font(.system(size: 50))
-                        Text("🍑")
-                            .font(.system(size: 50))
-                    }
-                    VStack (spacing: 37){
-                        Text("Apple")
+                    Image("Fruit")
+                    VStack (spacing: 15) {
+                        Text("Inventory")
                             .font(.system(size: 35))
-                        Text("Banana")
-                            .font(.system(size: 35))
-                        Text("Strawberry")
-                            .font(.system(size: 35))
-                        Text("Mango")
-                            .font(.system(size: 35))
-                        Text("Blueberry")
-                            .font(.system(size: 35))
-                        Text("Peach")
-                            .font(.system(size: 35))
-                    }
-                    VStack (spacing: 30) {
-                        Text("\(fruitsViewModel.numberofapples)")
-                            .font(.system(size: 40))
-                        Text("\(fruitsViewModel.numberofbananas)")
-                            .font(.system(size: 40))
-                        Text("\(fruitsViewModel.numberofstrawberries)")
-                            .font(.system(size: 40))
-                        Text("\(fruitsViewModel.numberofmangoes)")
-                            .font(.system(size: 40))
-                        Text("\(fruitsViewModel.numberofblueberries)")
-                            .font(.system(size: 40))
-                        Text("\(fruitsViewModel.numberofpeaches)")
-                            .font(.system(size: 40))
+                            .bold()
                         
+                        NavigationLink{
+                            carrotView()
+                        } label: {
+                            Text("Collect more fruits")
+                        }
+                        .padding()
+                        .font(.system(size: 27))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .maximum(300, 10))
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                        HStack (spacing: 30) {
+                            VStack (spacing: 20){
+                                Text("🍎")
+                                    .font(.system(size: 50))
+                                Text("🍌")
+                                    .font(.system(size: 50))
+                                Text("🍓")
+                                    .font(.system(size: 50))
+                                Text("🥭")
+                                    .font(.system(size: 50))
+                                Text("🫐")
+                                    .font(.system(size: 50))
+                                Text("🍑")
+                                    .font(.system(size: 50))
+                            }
+                            VStack (spacing: 37){
+                                Text("Apple")
+                                    .font(.system(size: 35))
+                                Text("Banana")
+                                    .font(.system(size: 35))
+                                Text("Strawberry")
+                                    .font(.system(size: 35))
+                                Text("Mango")
+                                    .font(.system(size: 35))
+                                Text("Blueberry")
+                                    .font(.system(size: 35))
+                                Text("Peach")
+                                    .font(.system(size: 35))
+                            }
+                            VStack (spacing: 30) {
+                                Text("\(fruitsViewModel.numberofapples)")
+                                    .font(.system(size: 40))
+                                Text("\(fruitsViewModel.numberofbananas)")
+                                    .font(.system(size: 40))
+                                Text("\(fruitsViewModel.numberofstrawberries)")
+                                    .font(.system(size: 40))
+                                Text("\(fruitsViewModel.numberofmangoes)")
+                                    .font(.system(size: 40))
+                                Text("\(fruitsViewModel.numberofblueberries)")
+                                    .font(.system(size: 40))
+                                Text("\(fruitsViewModel.numberofpeaches)")
+                                    .font(.system(size: 40))
+                                
+                            }
+                            
+                        }
+                        .frame(maxWidth: .maximum(340, 100))
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
                     }
-                    
                 }
-                .frame(maxWidth: .maximum(340, 100))
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(12)
-            }
         }
     }
 }
